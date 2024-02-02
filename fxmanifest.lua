1858820b7db466033ebb18a52857fa0eed0f4097
TabH2 <149743341+TabH2@users.noreply.github.com>
2024-02-01 18:29:02 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+games {'gta5' }
+
+author 'Tab'
+description 'Tab 911 Script'
+version '1.0.0'
+
+server_scripts {
+    'server.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+dependencies {
+    'nearest-postal'
+}
